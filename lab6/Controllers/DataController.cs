126e395dc90becf01439dc401a3f7098445529f9
Maria <maria.pozniak2013@gmail.com>
2024-11-24 21:11:17 +0200

    lab11 salesforce

--- lab6/Controllers/DataController.cs
+++ lab6/Controllers/DataController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using lab6.Data;
+using Newtonsoft.Json;
+using lab6.Services;
+using RestSharp;
 
 namespace lab6.Controllers
 {
@@ -10,10 +13,12 @@ namespace lab6.Controllers
     public class DataController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly ISalesforceAuthService _salesforceAuthService;
 
-        public DataController(ApplicationDbContext context)
+        public DataController(ApplicationDbContext context, ISalesforceAuthService salesforceAuthService)
         {
             _context = context;
+            _salesforceAuthService = salesforceAuthService;
         }
 
         [HttpGet("ProductsAndServices")]
@@ -35,6 +40,53 @@ namespace lab6.Controllers
         {
             var organizations = await _context.Organizations.ToListAsync();
             return Ok(organizations);
+        }
+
+        [HttpPost("sendAddresses")]
+        public async Task<ActionResult> SendAddresses()
+        {
+            var addresses = await _context.Organizations.ToListAsync();
+
+            var salesforceAddresses = addresses.Select(a => new
+            {
+                organizationId__c = a.OrganizationId,
+                organizationTypeCode__c = a.OrganizationTypeCode,
+                organizationDetails__c = a.OrganizationDetails
+            }).ToList();
+
+            var authResult = await _salesforceAuthService.AuthenticateAsync("maria.pozniak2013-ju1u@force.com", "maria.pozniak2013", "HWhsRfqe6KfraF9ORijBakN1");
+
+            if (!authResult.IsSuccess)
+            {
+                return Unauthorized($"Authentication failed with Salesforce. Error: {authResult.Result}");
+            }
+
+            string accessToken = authResult.AuthToken;
+            string instanceUrl = authResult.InstanceUrl;
+
+            string salesforceApiUrl = $"{instanceUrl}/services/data/v57.0/sobjects/Organizations/";
+
+            var client = new RestClient(salesforceApiUrl);
+
+            foreach (var address in salesforceAddresses)
+            {
+                var body = JsonConvert.SerializeObject(address);
+
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Authorization", "Bearer " + accessToken);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddJsonBody(body);
+
+                var response = await client.ExecuteAsync(request);
+
+                if (!response.IsSuccessful)
+                {
+                    return BadRequest($"Error sending data to Salesforce: {response.Content}");
+                }
+            }
+
+
+            return Ok("Data successfully sent to Salesforce.");
         }
 
         [HttpGet("Countries")]
---
+++ lab6/Services/SalesforceAuthService.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace lab6.Services
+{
+    public interface ISalesforceAuthService
+    {
+        Task<AuthResult> AuthenticateAsync(string username, string password, string securityToken);
+    }
+    public class SalesforceAuthService : ISalesforceAuthService
+    {
+        private readonly string ClientId = "3MVG9gYjOgxHsENJRCS5fJRltehlGXdiQIREClVXuP6O048.krThOGrtSlov_mwByoar4gNMTmlWkL7QA1VWY";
+        private readonly string ClientSecret = "31E6570F0DC977EC6E215FE79C81969CC7EAF846DA38CB0715FBA217085597D4";
+        private readonly string loginEndpoint = "https://login.salesforce.com/services/oauth2/token";
+        public async Task<AuthResult> AuthenticateAsync(string username, string password, string securityToken)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-www-form-urlencoded");
+                var requestData = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    {"grant_type", "password" },
+                    {"client_id", ClientId },
+                    {"client_secret", ClientSecret },
+                    {"username", username },
+                    {"password", $"{password}{securityToken}" }
+                });
+
+
+                var response = await client.PostAsync(loginEndpoint, requestData);
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonResponse);
+
+                    return new AuthResult
+                    {
+                        IsSuccess = true,
+                        AuthToken = values["access_token"],
+                        InstanceUrl = values["instance_url"]
+                    };
+                }
+                else
+                {
+                    var errorResponse = await response.Content.ReadAsStringAsync();
+
+                    return new AuthResult
+                    {
+                        IsSuccess = false,
+                        Result = errorResponse
+                    };
+                }
+            }
+        }
+    }
+    public class AuthResult
+    {
+        public bool IsSuccess { get; set; }
+        public string AuthToken { get; set; }
+        public string InstanceUrl { get; set; }
+        public string Result { get; set; }
+    }
+}
